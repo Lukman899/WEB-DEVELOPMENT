8c97a16b0bc22faf59d0ff4e89a84b7508790376
Lukman Shaikh <shaikhlukman899@gmail.com>
2026-09-04 20:47:39 +0530

    updated code

--- JAVA SCRIPT/LECTURE-38/coersion.js
+++ JAVA SCRIPT/LECTURE-38/coersion.js
@@ -1,7 +1,9 @@
+        //COERSION {CONVERSION OF TYPE}
+        // IMPLICIT COERSION
 // console.log("5" + 4);
 // console.log("2" * 5);
 // console.log(5 - "10");
-// console.log(true + 1); // true -> 1 ,fasle -> 0
+// console.log(true + 1); // true -> 1 ,false -> 0
 // console.log(true - true);
 // console.log(false - true);
 
@@ -10,14 +12,19 @@
 // console.log(result === NaN);
 // console.log(Number.isNaN(result));
 
+
+        // EXPLICIT COERSION
+
+
 // console.log(Number("5") + 4);
 
 // console.log(Number("5434h"));
 
 
-console.log(!![]); //true
-console.log(!!0); // false
-console.log(!!"0"); // true
+// console.log(!![]); //true
+// console.log(!!0); // false
+// console.log(!!"0"); // true
+
 
 
 // == ,=== 
