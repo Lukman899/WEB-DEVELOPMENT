516bb12b56cfd55ec74bdef176a27b5cf68b3752
Lukman Shaikh <shaikhlukman899@gmail.com>
2026-09-19 20:15:26 +0530

    LECTURE-47

--- JAVA SCRIPT/LECTURE-47/index.js
+++ JAVA SCRIPT/LECTURE-47/index.js
@@ -1,11 +1,14 @@
-// async function fun2(){
+"use strict"
+
+// async function fun2() {
 //     // console.log("Hii");
-//     return 11;
+//     return 11
 // }
 
-// function fun1(){
+// function fun1() {
 //     // console.log("Hello");
-//     // return promise.resolve(10)
+
+//     // return Promise.resolve(10)
 //     return 10
 // }
 
@@ -16,45 +19,162 @@
 // console.log(fun1());
 
 
+
+
 // console.log("a");
 
 // async function fun3(){
 //     console.log("b");
 // }
+
 // console.log("c");
-// fun3();
 
 
-async function fun3(){
-    return "hiiiiiii";
-}
-// fun3().then(data => {
-//     console.log(data);
-// });
+// fun3()
 
-async function fun4(){
-    // fun3().then(data => {
-    // console.log(data);
-    // })
 
-    let data = await fun3()
-    console.log(data);
-}
-fun4()
 
-// async function fun3(){
-//     return "hiiiiiii";
+
+// async function fun3() {
+//     return "hello"
 // }
-// // fun3().then(data => {
-// //     console.log(data);
-// // });
 
-// async function fun4(){
-//     fun3().then(data => {
-//     console.log(data);
-//     })
+// function fun4() {
+//     return Promise.resolve("hiiii")
+// }
+// console.log("1");
+// async function fun5() {
 
+//     // fun3().then(data => {
+//     //     console.log(data);
+//     // })
+
+//     console.log("2");
 //     let data = await fun3()
-//     console.log(data);
+//     console.log("3");
+//     let data2 = await fun4()
+//     console.log("4");
+//     console.log(data,data2);
 // }
-// fun4()
+
+// console.log("nishant");
+
+// fun5()
+
+// console.log("5");
+
+
+
+// console.log("a");
+
+// async function random() {
+
+
+//     console.log("b");
+
+//     await 1
+
+//     console.log("c");
+
+// }
+
+// random()
+
+// console.log("d");
+
+
+
+
+
+// let data;
+
+// async function userData() {
+//     return { name: "nishant" }
+// }
+
+// function fun4() {
+//     return Promise.reject("error aa gaya")
+// }
+
+// async function fun5() {
+//     try {
+//         data = await userData()
+//         let data2 = await fun4()
+//         console.log(data, data2);
+//     } catch (error) {
+//         console.log(error);
+//     } finally {
+//         console.log("mai toh hamesha run karunga");
+//     }
+// }
+
+// fun5()
+
+
+
+function searchPizza() {
+    return new Promise(function (resolve, reject) {
+        console.log("Pizza searching...");
+        setTimeout(function fun1() {
+            console.log("Here is the Pizza's Menu.");
+            let price = 500;
+            // a(price)
+            resolve(price)
+        }, 2000)
+    })
+}
+
+function addToCart() {
+    return new Promise(function (resolve, reject) {
+        console.log("Pizza adding to cart...");
+        setTimeout(function fun2() {
+            console.log("Pizza Added to cart");
+            resolve()
+        }, 3000)
+    })
+}
+
+function paymet(price) {
+    return new Promise(function (resolve, reject) {
+        console.log(`Payment Initiated , Amount : ${price}`);
+        setTimeout(function fun3() {
+
+            let isPaymentSuccessful = false
+
+            if (isPaymentSuccessful) {
+                console.log(`Payment Completed, Amount : ${price}`);
+                resolve()
+            } else {
+                reject("Bhaiya payment failed")
+            }
+
+
+        }, 5000)
+    })
+}
+
+// let res = searchPizza()
+
+// res.then(function (price) {
+//     return addToCart(price)
+// }).then(function (price) {
+//     return paymet(price)
+// }).then(function () {
+//     console.log("Bss Aa hee gaya Pizza");
+// }).catch(function (err) {
+//     console.log(err);
+// })
+
+
+async function orderFood() {
+    try {
+        let price = await searchPizza()
+        await addToCart()
+        await paymet(price)
+        console.log("Bss Aa hee gaya Pizza");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+orderFood()
--- JAVA SCRIPT/LECTURE-47/promiseMethods.js
+++ JAVA SCRIPT/LECTURE-47/promiseMethods.js
@@ -1,13 +1,35 @@
-function fun1(){
-    return Promise.resolve("fun1")
+
+
+
+function fun1() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject("fun1")
+        }, 3000)
+    })
 }
-function fun2(){
-    return Promise.resolve("fun2")
+
+function fun2() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject("fun2")
+        }, 5000)
+    })
 }
-function fun3(){
-    return Promise.resolve("fun3")
+
+function fun3() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject("fun3")
+        }, 0)
+    })
 }
-let result = Promise.all([fun1(), fun2(), fun3()]);
+
+// let result = Promise.all([fun1(), fun2(), fun3()])
+// let result = Promise.allSettled([fun1(), fun2(), fun3()]) 
+// let result = Promise.race([fun1(), fun2(), fun3()]) // jo phele settle ho gaya wo output hai
+let result = Promise.any([ fun1(), fun2(), fun3()]) //  jo phele fulfill ho gaya wo output hai
+
 result.then(data => {
     console.log(data);
 }).catch(err => {
