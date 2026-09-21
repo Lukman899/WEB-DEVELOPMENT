db375d19d446e7c5a05d11b1644dbbba083911f3
Lukman Shaikh <shaikhlukman899@gmail.com>
2026-09-21 11:21:13 +0530

    LECTURE 48

--- Velix GYM/index.js
+++ Velix GYM/index.js
@@ -1,28 +1,3 @@
-const themeBtn = document.querySelector("#themeBtn");
-
-// PAGE LOAD → saved theme check karo
-if (localStorage.getItem("theme") === "light") {
-    document.body.classList.add("dark-mode");
-    themeBtn.textContent = "☀️";
-} else {
-    document.body.classList.remove("dark-mode");
-    themeBtn.textContent = "🌙";
-}
-
-// BUTTON CLICK
-themeBtn.addEventListener("click", () => {
-
-    document.body.classList.toggle("dark-mode");
-
-    if (document.body.classList.contains("dark-mode")) {
-        themeBtn.textContent = "☀️";
-        localStorage.setItem("theme", "light");
-    } else {
-        themeBtn.textContent = "🌙";
-        localStorage.setItem("theme", "dark");
-    }
-
-});
 
 const getStartedBtns = document.querySelectorAll(".getstartbtn");
 
